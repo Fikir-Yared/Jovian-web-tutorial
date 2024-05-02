7a19120f2baa0036b1726284395b2b304798f690
Fikir-Yared <141673497+Fikir-Yared@users.noreply.github.com>
2024-05-02 18:14:12 +0000

    Added html template
    Added HTML template and working flask code

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+jobs = [{
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "Bengaluru, India",
+    "salary": "Birr. 10,000,000"
+}, {
+    "id": 2,
+    "title": "Hacker",
+    "location": "Addis Ababa, Ethiopia",
+    "salary": "Birr. 10,000"
+}, {
+    "id": 3,
+    "title": "Office Adminstrator",
+    "location": "Adama",
+    "salary": "Birr. 80,600"
+}]
 
 
 @app.route("/")
 def index():
-  return "<h1>Hello Worlsasdd</h1>"
+  return render_template("home.html", jobs=jobs)
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0")
+  app.run(host="0.0.0.0", debug=True)
